2f90cbb95641d509b62edff3b2458aa004f4c3a6
Justin Kerchal <jkerchal@earth.cs.colostate.edu>
2017-04-10 18:28:29 -0600

    image upload workinggit add database.php header.php upload.php

--- Project2/database.php
+++ Project2/database.php
@@ -1,26 +1,28 @@
 <?php
 	include 'header.php';
-	include 'dbconnect.php';
-        
-//         $dbh = new Database();
 //     class Database extends PDO{
 //         public function __construct(){
 //                 parent:: __construct("sqlite:" . __DIR__ . "./project2.sqlite");
 //         }
+
+//     include 'dbconnect.php';
         //functions
         function addIngredient($name, $price, $image){
+            $dbh = new PDO("sqlite:./project2.sqlite");
             $sql = "INSERT INTO ingredients(name, description, image) VALUES(?,?,?)";
-            $stm = $dbh -> prepare($sql);
-            return $stm -> execute(array($name, $price, $image));
+            $stm = $dbh->prepare($sql);
+            return $stm->execute(array($name, $price, $image));
         }
         function addComment($ingredient, $user, $comment, $id){
+            $dbh = new PDO("sqlite:./project2.sqlite");
             $sql = "INSERT INTO comments(ingredient, user, comment, id) VALUES(?, ?, ?, ?)";
-            $stm = $dbh -> prepare($sql);
-            return $stm -> execute(array($ingredient, $user, $comment, $id));
+            $stm = $dbh->prepare($sql);
+            return $stm->execute(array($ingredient, $user, $comment, $id));
         }
         function uploadIng($name, $price, $description, $image){
-            $dbh -> addIngredient($name, $price, NULL, $image);
-            $dbh -> addComment($ingredient, NULL, $description, NULL);
+            $dbh = new PDO("sqlite:./project2.sqlite");
+            addIngredient($name, $price, NULL, $image);
+            addComment($ingredient, NULL, $description, NULL);
         }
 //     }
 	include 'footer.php';    
